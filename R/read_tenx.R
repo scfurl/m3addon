2c987da5028ecd2647d5286ca644bb7a7156741c
scfurl <sfurlan@uw.edu>
2019-07-18 08:31:07 -0700

    #completed testing of functions that make cds out of h5 data

--- R/read_tenx.R
+++ R/read_tenx.R
@@ -1,6 +1,6 @@
-
+#' @importFrom h5 h5file
 read.cds.cellranger.h5.file = function(h5.file) {
-  s<-h5::h5file(h5.file)
+  s<-h5file(h5.file)
   barcodes = readDataSet(s["matrix"]["barcodes"])
   gene_ids = readDataSet(s["matrix"]["features"]["id"])
   gene_names =readDataSet(s["matrix"]["features"]["name"])
@@ -41,8 +41,8 @@ read.cds.cellranger.h5.file = function(h5.file) {
   return(res)
 }
 
-
-#' @description #This function reads a vector of cellranger "out" folders for .h5 files.  This function
+#' Make monocle3 cell_data_set using cellranger h5
+#' @description This function reads a vector of cellranger "out" folders for .h5 files.  This function
 #' will create a cds of the data using cellranger thresholds for filtering out droplets that do not contain cells.
 #' Optionally, this function can return the unfiltered data as well for further manipulation (ie adjustment of 
 #' droplet thresholds) if desired.
@@ -59,10 +59,10 @@ read.cds.cellranger.h5.file = function(h5.file) {
 #' @param expressed_genes If true, this option removes genes that do not have expression in at least a 
 #' minumum number of cells (cell_min parameter)
 #' @param cell_min Minimum number of cells that a gene needs to be expressed for the gene to be included in the 
-#' @param aggregated will read a cellranger aggregate folder, this is currently only supported for 1 folder
-@param cds the cell_data_set upon which to perform this operation.
-
+#' @param aggregated will read a cellranger aggregate folder, this is currently only supported for 1 folder.  The 'aggregation.csv'
+#' file must be included in the 'outs' folder.
 #' @return a cell_data_set object or a list of items if unfiltered data is returned (see unfiltered)
+#' @importFrom h5 h5file
 #' @export
 load_cellranger_data_h5<-function(folders, 
                                   samplenames=NULL, 
@@ -76,11 +76,18 @@ load_cellranger_data_h5<-function(folders,
   #return the unfiltered data as well.  
   if(aggregated){
     if(length(folders)>1) stop("Only one aggregated folder supported")
-    message(paste0("Reading data for: ", folders[1]))
+    message(paste0("Reading aggregated data for: ", folders[1]))
     if(!file.exists(file.path(folders, "outs", "aggregation.csv"))) stop("aggregation.csv file must be present in 'outs'")
     cds = read.cds.cellranger.h5.file(
       file.path(folders[1], "outs", "raw_feature_bc_matrix.h5"))
     agg<-read.csv(file.path(folders[1], "outs", "aggregation.csv"))
+    message(paste0("Finding threshold for aggregegated data: ", folders[1]))
+    pData(cds)$n.umi = Matrix::colSums(exprs(cds))
+    h5.file<-file.path(folders[1], "outs", "filtered_feature_bc_matrix.h5")
+    s<-h5file(h5.file)
+    umi.count.thresholds.from.cellranger = pData(cds)$n.umi[order(-pData(cds)$n.umi)][readDataSet(s["matrix"]["shape"])[2]]
+    umi.count.thresholds = umi.count.thresholds.from.cellranger
+    cds = cds[,pData(cds)$n.umi >= umi.count.thresholds                                                                ]
     pData(cds)$sample_no<-sapply(strsplit(rownames(pData(cds)), "-"), "[[", 2)
     agg$sample_no<-1:nrow(agg)
     if(expressed_genes){
@@ -122,7 +129,7 @@ load_cellranger_data_h5<-function(folders,
     #   umi.cdf(pData(unfiltered.cds.list[[sample.id]])$n.umi)
 
     h5.file<-file.path(sample.id, "outs", "filtered_feature_bc_matrix.h5")
-    s<-h5::h5file(h5.file)
+    s<-h5file(h5.file)
     umi.count.thresholds.from.cellranger[[sample.id]] = pData(unfiltered.cds.list[[sample.id]])$n.umi[order(-pData(unfiltered.cds.list[[sample.id]])$n.umi)][readDataSet(s["matrix"]["shape"])[2]]
 
     umi.count.thresholds[[sample.id]] = umi.count.thresholds.from.cellranger[[sample.id]]
@@ -186,11 +193,30 @@ load_cellranger_data_h5<-function(folders,
   cds
 }
 
+#' Add colData (aka pData) to a cell_data_set
+#' @description This function will take a cds and add sample levels phenodata to all the cells from that 
+#sample in the cds.  The function will match sample names from the cds_anchor parameter to 
+#the sample names from the df_anchor parameter and add the remaining phenodata from the df 
+#to the cds.
+#'
+#' @param folders A vector of cellranger folders (full folder name is ideal).  These must contain an "outs" subfolders with
+#' two files; 1) raw_feature_bc_matrix.h5, and 2) filtered_feature_bc_matrix.h5.  For aggregated samples
+#' run with cellrangers 'aggregate' function, this folder should also contain the aggregation.csv file.
+#' @param samplenames An optional vector that corresponds to the names you would like to give to
+#'each element in filelist.  This defaults to the the basename of the filelist element i.e.
+# the folder /home/user/cellranger/runs/Samp1-SA-GA_A1 would be given the name: "Samp1-SA-GA_A1"
+#' @param unfiltered This parameter, if true, returns a list containing 1) the filtered cds,
+#'2) a list of unfiltered cds for each sample in 'folders'.
+#' @param empty.droplet.threshold minimum number of umis per droplet to include in unfiltered output (default 15)
+#' @param expressed_genes If true, this option removes genes that do not have expression in at least a 
+#' minumum number of cells (cell_min parameter)
+#' @param cell_min Minimum number of cells that a gene needs to be expressed for the gene to be included in the 
+#' @param aggregated will read a cellranger aggregate folder, this is currently only supported for 1 folder.  The 'aggregation.csv'
+#' file must be included in the 'outs' folder.
+#' @return a cell_data_set object or a list of items if unfiltered data is returned (see unfiltered)
+#' @export
 add_meta_data_cds<-function(cds, meta, cds_col="sample", meta_col="sample"){
-  #This function will take a cds and add sample levels phenodata to all the cells from that 
-  #sample in the cds.  The function will match sample names from the cds_anchor parameter to 
-  #the sample names from the df_anchor parameter and add the remaining phenodata from the df 
-  #to the cds.
+
   cds_col<-as.character(cds_col)
   meta_col<-as.character(meta_col)
   if(class(cds)!="cell_data_set"){stop("input 'cds' is not a monocle3 cell data set")}
@@ -204,6 +230,7 @@ add_meta_data_cds<-function(cds, meta, cds_col="sample", meta_col="sample"){
   pData(cds)<-cbind(pData(cds), add)
   return(cds)
 }
+
 
 all.identical <- function(l) all(mapply(identical, head(l, 1), tail(l, -1)))
 
