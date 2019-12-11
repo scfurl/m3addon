711de4747e374d455e35b3af11f6c4f884a143a1
scfurl <sfurlan@uw.edu>
2019-12-10 16:24:46 -0800

    trimap working

--- R/trimap.R
+++ R/trimap.R
@@ -18,19 +18,24 @@
 #' @export
 #' @importFrom reticulate source_python
 #' @references TriMap: Large-scale Dimensionality Reduction Using Triplets. E Amid, MK Warmuth - arXiv preprint arXiv:1910.00204, 2019 - arxiv.org
-trimap<-function(cds, python_home = system("which python", intern = TRUE), module_file=paste(system.file(package = "m3addon"),"trimap.py", sep = "/"),
+trimap<-function(cds, python_home = system("which python", intern = TRUE), num_dims = NULL, module_file=paste(system.file(package = "m3addon"),"trimap.py", sep = "/"),
                   n_dims = 2, n_inliers= 10, n_outliers = 5, n_random = 5, distance = c('euclidean', 'manhattan', 'angular', 'hamming'), 
-                  lr = 1000.0, n_iters = 400, knn_tuple = NULL, apply_pca = TRUE, opt_method = c('dbd', 'sd',  'momentum'), 
+                  lr = 1000.0, n_iters = 400, knn_tuple = NULL, apply_pca_trimap = FALSE, opt_method = c('dbd', 'sd',  'momentum'), 
                   verbose = TRUE, weight_adj = 500.0,  return_seq = FALSE)
 {
   #reticulate::use_python(python_home)
   source_python(module_file)
-  data<-t(as.matrix(exprs(cds)))
+  if(is.null(reducedDims(cds)["PCA"]))stop("PCA not found in the reducedDims slot")
+  if(is.null(num_dims)){
+    num_dims<-dim(reducedDims(cds)["PCA"][[1]])[2]
+  }
+  data <- reducedDims(cds)["PCA"][[1]][,1:num_dims]
+  #data<-t(as.matrix(exprs(cds)))
   distance <- match.arg(distance)
   opt_method <- match.arg(opt_method)
-  trimap_args<-c(list(data, n_dims, n_inliers, n_outliers, n_random, distance, lr, n_iters, knn_tuple, apply_pca, opt_method,
+  trimap_args<-c(list(data, n_dims, n_inliers, n_outliers, n_random, distance, lr, n_iters, knn_tuple, apply_pca_trimap, opt_method,
                                       verbose, weight_adj, return_seq))
-  res <- do.call(trimap_fromR, trimap_args)
-  return(res)
+  reducedDims(cds)[["trimap"]] <- do.call(trimap_fromR, trimap_args)
+  return(cds)
 }
 
