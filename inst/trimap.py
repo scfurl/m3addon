a80d95b9d12341b81b15592e64d0441f9e091d41
scfurl <sfurlan@uw.edu>
2019-12-10 16:00:25 -0800

    bugs

--- inst/trimap.py
+++ inst/trimap.py
@@ -1,6 +1,6 @@
 def trimap_fromR(data, n_dims, n_inliers, n_outliers, n_random, distance, lr, n_iters, knn_tuple, apply_pca, opt_method, verbose, weight_adj, return_seq):
   import trimap
   knn_tuple=None
-  embedding = trimap.TRIMAP(int(n_dims), int(n_inliers), int(n_outliers), int(n_random), str(distance), float(lr), int(n_iters), knn_tuple, bool(apply_pca), str(opt_method), bool(verbose), float(weight_adj), bool(return_seq)).fit_transform(data)
+  embedding = trimap.TRIMAP(n_dims = int(n_dims), n_inliers = int(n_inliers), n_outliers = int(n_outliers), n_random = int(n_random), distance = str(distance), lr = float(lr), n_iters = int(n_iters), apply_pca = bool(apply_pca),opt_method = str(opt_method), verbose = bool(verbose), weight_adj = float(weight_adj), return_seq = bool(return_seq)).fit_transform(data)
   return(embedding)
         
