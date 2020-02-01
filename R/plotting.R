6a0cc91b0e571de4fedda334db5d79965508d910
scfurl <sfurlan@uw.edu>
2020-01-31 18:49:41 -0800

    smooth curves function

--- R/plotting.R
+++ R/plotting.R
@@ -1132,3 +1132,242 @@ plot_heatmap <- function (cds, markers, sample_cell_num = NULL, group_by = "Clus
   }
 }
 
+generate_smooth_curves<-function (cds, new_data, trend_formula = "~sm.ns(Pseudotime, df = 3)", 
+          relative_expr = T, response_type = "response", cores = 1) 
+{
+  expressionFamily <- cds@expressionFamily
+  if (cores > 1) {
+    expression_curve_matrix <- mcesApply(cds, 1, function(x, 
+                                                          trend_formula, expressionFamily, relative_expr, new_data, 
+                                                          fit_model_helper, responseMatrix, calculate_NB_dispersion_hint, 
+                                                          calculate_QP_dispersion_hint) {
+      environment(fit_model_helper) <- environment()
+      environment(responseMatrix) <- environment()
+      model_fits <- fit_model_helper(x, modelFormulaStr = trend_formula, 
+                                     expressionFamily = expressionFamily, relative_expr = relative_expr, 
+                                     disp_func = cds@dispFitInfo[["blind"]]$disp_func)
+      if (is.null(model_fits)) 
+        expression_curve <- as.data.frame(matrix(rep(NA, 
+                                                     nrow(new_data)), nrow = 1))
+      else expression_curve <- as.data.frame(responseMatrix(list(model_fits), 
+                                                            newdata = new_data, response_type = response_type))
+      colnames(expression_curve) <- row.names(new_data)
+      expression_curve
+    }, required_packages = c("BiocGenerics", "Biobase", "VGAM", 
+                             "plyr"), cores = cores, trend_formula = trend_formula, 
+    expressionFamily = expressionFamily, relative_expr = relative_expr, 
+    new_data = new_data, fit_model_helper = fit_model_helper, 
+    responseMatrix = responseMatrix, calculate_NB_dispersion_hint = calculate_NB_dispersion_hint, 
+    calculate_QP_dispersion_hint = calculate_QP_dispersion_hint)
+    expression_curve_matrix <- as.matrix(do.call(rbind, expression_curve_matrix))
+    return(expression_curve_matrix)
+  }
+  else {
+    expression_curve_matrix <- smartEsApply(cds, 1, function(x, 
+                                                             trend_formula, expressionFamily, relative_expr, new_data) {
+      environment(fit_model_helper) <- environment()
+      environment(responseMatrix) <- environment()
+      model_fits <- fit_model_helper(x, modelFormulaStr = trend_formula, 
+                                     expressionFamily = expressionFamily, relative_expr = relative_expr, 
+                                     disp_func = cds@dispFitInfo[["blind"]]$disp_func)
+      if (is.null(model_fits)) 
+        expression_curve <- as.data.frame(matrix(rep(NA, 
+                                                     nrow(new_data)), nrow = 1))
+      else expression_curve <- as.data.frame(responseMatrix(list(model_fits), 
+                                                            new_data, response_type = response_type))
+      colnames(expression_curve) <- row.names(new_data)
+      expression_curve
+    }, convert_to_dense = TRUE, trend_formula = trend_formula, 
+    expressionFamily = expressionFamily, relative_expr = relative_expr, 
+    new_data = new_data)
+    expression_curve_matrix <- as.matrix(do.call(rbind, expression_curve_matrix))
+    row.names(expression_curve_matrix) <- row.names(fData(cds))
+    return(expression_curve_matrix)
+  }
+}
+
+#' Plots a pseudotime-ordered, row-centered heatmap
+#' 
+#' @description The function plot_pseudotime_heatmap takes a CellDataSet object 
+#' (usually containing a only subset of significant genes) and generates smooth expression 
+#' curves much like plot_genes_in_pseudotime. 
+#' Then, it clusters these genes and plots them using the pheatmap package. 
+#' This allows you to visualize modules of genes that co-vary across pseudotime.
+#' 
+#' @param cds_subset CellDataSet for the experiment 
+#' @param cluster_rows Whether to cluster the rows of the heatmap.
+#' @param hclust_method The method used by pheatmap to perform hirearchical clustering of the rows. 
+#' @param num_clusters Number of clusters for the heatmap of branch genes
+#' @param hmcols The color scheme for drawing the heatmap.
+#' @param add_annotation_row Additional annotations to show for each row in the heatmap. Must be a dataframe with one row for each row in the fData table of cds_subset, with matching IDs.
+#' @param add_annotation_col Additional annotations to show for each column in the heatmap. Must be a dataframe with one row for each cell in the pData table of cds_subset, with matching IDs.
+#' @param show_rownames Whether to show the names for each row in the table.
+#' @param use_gene_short_name Whether to use the short names for each row. If FALSE, uses row IDs from the fData table.
+#' @param scale_max The maximum value (in standard deviations) to show in the heatmap. Values larger than this are set to the max.
+#' @param scale_min The minimum value (in standard deviations) to show in the heatmap. Values smaller than this are set to the min.
+#' @param norm_method Determines how to transform expression values prior to rendering
+#' @param trend_formula A formula string specifying the model used in fitting the spline curve for each gene/feature.
+#' @param return_heatmap Whether to return the pheatmap object to the user. 
+#' @param cores Number of cores to use when smoothing the expression curves shown in the heatmap.
+#' @return A list of heatmap_matrix (expression matrix for the branch committment), ph (pheatmap heatmap object),
+#' annotation_row (annotation data.frame for the row), annotation_col (annotation data.frame for the column). 
+#' @import pheatmap
+#' @importFrom stats sd as.dist cor cutree
+#' @export
+#'
+
+plot_pseudotime_heatmap <- function(cds_subset, 
+                                    
+                                    cluster_rows = TRUE,
+                                    hclust_method = "ward.D2", 
+                                    num_clusters = 6,
+                                    
+                                    hmcols = NULL, 
+                                    
+                                    add_annotation_row = NULL,
+                                    add_annotation_col = NULL,
+                                    show_rownames = FALSE, 
+                                    use_gene_short_name = TRUE,
+                                    
+                                    norm_method = c("log", "vstExprs"), 
+                                    scale_max=3, 
+                                    scale_min=-3, 
+                                    
+                                    trend_formula = '~sm.ns(Pseudotime, df=3)',
+                                    
+                                    return_heatmap=FALSE,
+                                    cores=1){
+  num_clusters <- min(num_clusters, nrow(cds_subset))
+  pseudocount <- 1
+  newdata <- data.frame(Pseudotime = seq(min(cds_subset@principal_graph_aux$UMAP$pseudotime), max(cds_subset@principal_graph_aux$UMAP$pseudotime),length.out = 100)) 
+  m <- genSmoothCurves(cds_subset, cores=cores, trend_formula = trend_formula,  
+                       relative_expr = T, new_data = newdata)
+  
+  
+  #remove genes with no expression in any condition
+  m=m[!apply(m,1,sum)==0,]
+  
+  norm_method <- match.arg(norm_method)
+  
+  # FIXME: this needs to check that vst values can even be computed. (They can only be if we're using NB as the expressionFamily)
+  if(norm_method == 'vstExprs' && is.null(cds_subset@dispFitInfo[["blind"]]$disp_func) == FALSE) {
+    m = vstExprs(cds_subset, expr_matrix=m)
+  }     
+  else if(norm_method == 'log') {
+    m = log10(m+pseudocount)
+  }
+  
+  # Row-center the data.
+  m=m[!apply(m,1,sd)==0,]
+  m=Matrix::t(scale(Matrix::t(m),center=TRUE))
+  m=m[is.na(row.names(m)) == FALSE,]
+  m[is.nan(m)] = 0
+  m[m>scale_max] = scale_max
+  m[m<scale_min] = scale_min
+  
+  heatmap_matrix <- m
+  
+  row_dist <- as.dist((1 - cor(Matrix::t(heatmap_matrix)))/2)
+  row_dist[is.na(row_dist)] <- 1
+  
+  if(is.null(hmcols)) {
+    bks <- seq(-3.1,3.1, by = 0.1)
+    hmcols <- blue2green2red(length(bks) - 1)
+  }
+  else {
+    bks <- seq(-3.1,3.1, length.out = length(hmcols))
+  } 
+  
+  ph <- pheatmap(heatmap_matrix, 
+                 useRaster = T,
+                 cluster_cols=FALSE, 
+                 cluster_rows=cluster_rows, 
+                 show_rownames=F, 
+                 show_colnames=F, 
+                 clustering_distance_rows=row_dist,
+                 clustering_method = hclust_method,
+                 cutree_rows=num_clusters,
+                 silent=TRUE,
+                 filename=NA,
+                 breaks=bks,
+                 border_color = NA,
+                 color=hmcols)
+  
+  if(cluster_rows) {
+    annotation_row <- data.frame(Cluster=factor(cutree(ph$tree_row, num_clusters)))
+  } else {
+    annotation_row <- NULL
+  }
+  
+  if(!is.null(add_annotation_row)) {
+    old_colnames_length <- ncol(annotation_row)
+    annotation_row <- cbind(annotation_row, add_annotation_row[row.names(annotation_row), ])  
+    colnames(annotation_row)[(old_colnames_length+1):ncol(annotation_row)] <- colnames(add_annotation_row)
+    # annotation_row$bif_time <- add_annotation_row[as.character(fData(absolute_cds[row.names(annotation_row), ])$gene_short_name), 1]
+  }
+  
+  if(!is.null(add_annotation_col)) {
+    if(nrow(add_annotation_col) != 100) {
+      stop('add_annotation_col should have only 100 rows (check genSmoothCurves before you supply the annotation data)!')
+    }
+    annotation_col <- add_annotation_col
+  } else {
+    annotation_col <- NA
+  }
+  
+  if (use_gene_short_name == TRUE) {
+    if (is.null(fData(cds_subset)$gene_short_name) == FALSE) {
+      feature_label <- as.character(fData(cds_subset)[row.names(heatmap_matrix), 'gene_short_name'])
+      feature_label[is.na(feature_label)] <- row.names(heatmap_matrix)
+      
+      row_ann_labels <- as.character(fData(cds_subset)[row.names(annotation_row), 'gene_short_name'])
+      row_ann_labels[is.na(row_ann_labels)] <- row.names(annotation_row)
+    }
+    else {
+      feature_label <- row.names(heatmap_matrix)
+      row_ann_labels <- row.names(annotation_row)
+    }
+  }
+  else {
+    feature_label <- row.names(heatmap_matrix)
+    if(!is.null(annotation_row))
+      row_ann_labels <- row.names(annotation_row)
+  }
+  
+  row.names(heatmap_matrix) <- feature_label
+  if(!is.null(annotation_row))
+    row.names(annotation_row) <- row_ann_labels
+  
+  colnames(heatmap_matrix) <- c(1:ncol(heatmap_matrix))
+  
+  ph_res <- pheatmap(heatmap_matrix[, ], #ph$tree_row$order
+                     useRaster = T,
+                     cluster_cols = FALSE, 
+                     cluster_rows = cluster_rows, 
+                     show_rownames=show_rownames, 
+                     show_colnames=F, 
+                     #scale="row",
+                     clustering_distance_rows=row_dist, #row_dist
+                     clustering_method = hclust_method, #ward.D2
+                     cutree_rows=num_clusters,
+                     # cutree_cols = 2,
+                     annotation_row=annotation_row,
+                     annotation_col=annotation_col,
+                     treeheight_row = 20, 
+                     breaks=bks,
+                     fontsize = 6,
+                     color=hmcols, 
+                     border_color = NA,
+                     silent=TRUE,
+                     filename=NA
+  )
+  
+  grid::grid.rect(gp=grid::gpar("fill", col=NA))
+  grid::grid.draw(ph_res$gtable)
+  if (return_heatmap){
+    return(ph_res)
+  }
+}
+
+
+
