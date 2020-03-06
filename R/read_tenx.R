219d50316e2a40d991d39a8c7c7eb65f09abfa8f
Scott Furlan <sfurlan@c02c315rjv40.fhcrc.org>
2020-03-06 14:59:31 -0800

    comma bug

--- R/read_tenx.R
+++ R/read_tenx.R
@@ -89,8 +89,8 @@ load_cellranger_data_h5<-function(folders,
                                   ){
   #This function reads a vector of cellranger folder "out" folders for .h5 files.  Optionally can
   #return the unfiltered data as well.  
-  if(!chemistry %in% c("threeprime" "fiveprime" "SC3Pv1" "SC3Pv2" "SC3Pv3" "SC5P-PE", "SC5P-R2")){stop("Chemistry not found")}
-  if(chemistry %in% c("threeprime" "fiveprime" "SC3Pv1" "SC3Pv2" "SC3Pv3" "SC5P-PE", "SC5P-R2")){
+  if(!chemistry %in% c("threeprime", "fiveprime", "SC3Pv1", "SC3Pv2", "SC3Pv3", "SC5P-PE", "SC5P-R2")){stop("Chemistry not found")}
+  if(chemistry %in% c("threeprime", "fiveprime", "SC3Pv1", "SC3Pv2", "SC3Pv3", "SC5P-PE", "SC5P-R2")){
     if(aggregated){
       #aggregate filtered option:
       if(length(folders)>1) stop("Only one aggregated folder supported")
