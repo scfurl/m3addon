cc5aadbb3397602a3c030be1b091dba99727d08e
Scott Furlan <sfurlan@c02c315rjv40.fhcrc.org>
2020-03-12 14:21:22 -0700

    moved cpp funciton

--- R/reduce_dimensions.R
+++ R/reduce_dimensions.R
@@ -250,3 +250,31 @@ iterative_LSI <- function(cds,
   }
   cds
 }
+
+
+#Sparse Variances Rcpp
+Rcpp::sourceCpp(code='
+  #include <Rcpp.h>
+  using namespace Rcpp;
+  using namespace std;
+  // [[Rcpp::export]]
+  Rcpp::NumericVector computeSparseRowVariances(IntegerVector j, NumericVector val, NumericVector rm, int n) {
+    const int nv = j.size();
+    const int nm = rm.size();
+    Rcpp::NumericVector rv(nm);
+    Rcpp::NumericVector rit(nm);
+    int current;
+    // Calculate RowVars Initial
+    for (int i = 0; i < nv; ++i) {
+      current = j(i) - 1;
+      rv(current) = rv(current) + (val(i) - rm(current)) * (val(i) - rm(current));
+      rit(current) = rit(current) + 1;
+    }
+    // Calculate Remainder Variance
+    for (int i = 0; i < nm; ++i) {
+      rv(i) = rv(i) + (n - rit(i))*rm(i)*rm(i);
+    }
+    rv = rv / (n - 1);
+    return(rv);
+  }'
+)
