99db1982c369eca8466af43dac7345698a7a866a
scfurl <sfurlan@uw.edu>
2020-03-16 11:23:52 -0700

    wrote load STARsolo function

--- R/read_tenx.R
+++ R/read_tenx.R
@@ -57,12 +57,53 @@ read.cds.cellranger.h5.file = function(h5.file) {
   return(res)
 }
 
+
+
+#' @importFrom Matrix sparseMatrix
+#' @importFrom Matrix readMM
+#' @importFrom data.table fread
+read.cds.starsolo.file = function(folder) {
+  if(!file.exists(folder)){stop(paste0("File ", folder," not found"))}
+  files<-c("features.tsv", "barcodes.tsv", "matrix.mtx")
+  
+  barcodes = fread(file.path(folder, files[2]), header = F)$V1
+  feats<- fread(file.path(folder, files[1]), header=F)
+  gene_ids = feats$V1
+  gene_names = feats$V2
+  gbm = readMM(file.path(folder, files[3]))
+  
+  
+  pData.df = data.frame(
+    barcode = barcodes,
+    stringsAsFactors = F
+  )
+  
+  fData.df = data.frame(
+    id = gene_ids,
+    gene_short_name = gene_names,
+    stringsAsFactors = F
+  )
+  
+  rownames(pData.df) = pData.df$barcode
+  rownames(fData.df) = fData.df$id
+  
+  rownames(gbm) = rownames(fData.df)
+  colnames(gbm) = rownames(pData.df)
+  
+  suppressWarnings({res = new_cell_data_set(
+    gbm,
+    cell_metadata = pData.df,
+    gene_metadata = fData.df
+  )})
+  
+  return(res)
+}
+
 #' Make monocle3 cell_data_set using cellranger h5
 #' @description This function reads a vector of cellranger "out" folders for .h5 files.  This function
 #' will create a cds of the data using cellranger thresholds for filtering out droplets that do not contain cells.
 #' Optionally, this function can return the unfiltered data as well for further manipulation (ie adjustment of 
 #' droplet thresholds) if desired.
-#'
 #' @param folders A vector of cellranger folders (full folder name is ideal).  These must contain an "outs" subfolders with
 #' two files; 1) raw_feature_bc_matrix.h5, and 2) filtered_feature_bc_matrix.h5.  For aggregated samples
 #' run with cellrangers 'aggregate' function, this folder should also contain the aggregation.csv file.
@@ -366,6 +407,149 @@ load_cellranger_data_h5<-function(folders,
   }
 }
 
+
+#' Make monocle3 cell_data_set using STARsolo
+#' @description This function reads a vector of STARsolo "out" folders for .mtx and .tsv files.  This function
+#' will create a cds of the data using cellranger thresholds for filtering out droplets that do not contain cells.
+#' Optionally, this function can return the unfiltered data as well for further manipulation (ie adjustment of 
+#' droplet thresholds) if desired.
+#' @param folders A vector of STARsolo folders (full folder name is ideal).  These must contain an "outs" subfolders with
+#' three files: 1) features.tsv, 2) barcodes.tsv, and 3) matrix.mtx.
+#' @param samplenames An optional vector that corresponds to the names you would like to give to
+#'each element in filelist.
+#' @param count_method (required) - A string denoting STARsolo output counting method.  (one of either: Gene, GeneFull, SJ, Velocyto)
+#' @param unfiltered This parameter, if true, returns a list containing 1) the filtered cds,
+#'2) a list of unfiltered cds for each sample in 'folders'.
+#' @param empty.droplet.threshold minimum number of umis per droplet to include in unfiltered output (default 15)
+#' @param expressed_genes If true, this option removes genes that do not have expression in at least a 
+#' minumum number of cells (cell_min parameter)
+#' @param cell_min Minimum number of cells that a gene needs to be expressed for the gene to be included in the 
+#' @return a cell_data_set object or a list of items if unfiltered data is returned (see unfiltered)
+#' @importFrom Matrix colSums
+#' @export
+load_STARsolo_data<-function(folders, 
+                                  samplenames=NULL, 
+                                  unfiltered=F, 
+                                  empty.droplet.threshold=15, 
+                                  expressed_genes=TRUE, count_method="Gene",
+                                  cell_min=1){
+    
+
+    if(is.null(samplenames)){
+      sample.ids<-folders
+      names(sample.ids)<-sapply(folders, basename)
+    }else{
+      sample.ids<-folders
+      names(sample.ids)<-samplenames
+    }
+    
+    ######filt
+    #read filtered data
+    filtered.cds.list<-list()
+    for(sample.id in sample.ids){
+      message(paste0("Reading (filtered) data for: ", sample.id))
+      filtered.cds.list[[sample.id]] = read.cds.starsolo.file(
+        file.path(sample.id, "outs", count_method, "filtered"))
+      pData(filtered.cds.list[[sample.id]])$n.umi<-colSums(exprs(filtered.cds.list[[sample.id]]))
+    }
+    
+    #add_and checkrownames
+    if(length(filtered.cds.list)>1){
+      fdat_rownames<-lapply(filtered.cds.list, function(cds) rownames(fData(cds)))
+      if(!all.identical(fdat_rownames))stop("Not all genes are the same across samples")
+    }
+    #make fData
+    common.fData = fData(filtered.cds.list[[sample.ids[1]]])
+    names(filtered.cds.list)<-names(sample.ids)
+    #make pData
+    new.pData = list()
+    for (sample.id in names(sample.ids)) {
+      new.pData[[sample.id]] = pData(filtered.cds.list[[sample.id]])
+      
+      new.pData[[sample.id]]$sample = sample.id
+      
+      new.pData[[sample.id]]$cell = paste(
+        sample.id, new.pData[[sample.id]]$barcode, sep = ".")
+      
+      rownames(new.pData[[sample.id]]) = new.pData[[sample.id]]$cell
+    }
+    #make exprsData
+    new.exprs = list()
+    for (sample.id in names(sample.ids)) {
+      new.exprs[[sample.id]] = exprs(filtered.cds.list[[sample.id]])
+      colnames(new.exprs[[sample.id]]) = new.pData[[sample.id]]$cell
+    }
+    
+    #combine
+    combined.pData = do.call(rbind, new.pData)
+    combined.pData = combined.pData[, c("barcode", "n.umi",  "sample")]
+    rownames(combined.pData) = paste0(combined.pData$sample, ".", combined.pData$barcode)
+    combined.exprs = do.call(cbind, new.exprs)
+    cds = new_cell_data_set(
+      combined.exprs,
+      cell_metadata =  combined.pData,
+      gene_metadata = common.fData
+    )
+    if(expressed_genes){
+      cds<-detect_genes(cds, exprs_bin = F)
+      cds<-cds[fData(cds)$num_cells_expressed>cell_min,]
+    }
+    if(!unfiltered){return(cds)}
+    
+    #####unfilt
+    #read unfiltered data
+    unfiltered.cds.list<-list()
+    for(sample.id in sample.ids){
+      message(paste0("Reading (unfiltered) data for: ", sample.id))
+      unfiltered.cds.list[[sample.id]] = read.cds.starsolo.file(
+        file.path(sample.id, "outs", count_method, "raw"))
+      pData(unfiltered.cds.list[[sample.id]])$n.umi<-colSums(exprs(unfiltered.cds.list[[sample.id]]))
+    }
+    
+    #add_and checkrownames
+    if(length(filtered.cds.list)>1){
+      fdat_rownames<-lapply(filtered.cds.list, function(cds) rownames(fData(cds)))
+      if(!all.identical(fdat_rownames))stop("Not all genes are the same across samples")
+    }
+    #make fData
+    common.fData = fData(unfiltered.cds.list[[sample.ids[1]]])
+    names(unfiltered.cds.list)<-names(sample.ids)
+    #make pData
+    new.pData = list()
+    for (sample.id in names(sample.ids)) {
+      new.pData[[sample.id]] = pData(unfiltered.cds.list[[sample.id]])
+      
+      new.pData[[sample.id]]$sample = sample.id
+      
+      new.pData[[sample.id]]$cell = paste(
+        sample.id, new.pData[[sample.id]]$barcode, sep = ".")
+      
+      rownames(new.pData[[sample.id]]) = new.pData[[sample.id]]$cell
+    }
+    #make exprsData
+    new.exprs = list()
+    for (sample.id in names(sample.ids)) {
+      new.exprs[[sample.id]] = exprs(unfiltered.cds.list[[sample.id]])
+      colnames(new.exprs[[sample.id]]) = new.pData[[sample.id]]$cell
+    }
+    
+    #combine
+    combined.pData = do.call(rbind, new.pData)
+    combined.pData = combined.pData[, c("barcode", "n.umi",  "sample")]
+    rownames(combined.pData) = paste0(combined.pData$sample, ".", combined.pData$barcode)
+    combined.exprs = do.call(cbind, new.exprs)
+    cds_unfilt = new_cell_data_set(
+      combined.exprs,
+      cell_metadata =  combined.pData,
+      gene_metadata = common.fData
+    )
+    if(expressed_genes){
+      cds_unfilt<-detect_genes(cds_unfilt, exprs_bin = F)
+      cds_unfilt<-cds_unfilt[fData(cds_unfilt)$num_cells_expressed>cell_min,]
+    }
+    if(unfiltered){return(list(unfiltered_cds=cds_unfilt, filtered_cds=cds))}
+}
+ 
 
 #' Add colData (aka pData) to a cell_data_set
 #' @description This function will take a cds and add sample levels phenodata to all the cells from that 
